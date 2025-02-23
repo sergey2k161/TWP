77a67bcf3c2a343d1032ac4e3d07bf931cef3c7a
sergey2k161 <you@example.com>
2025-02-23 23:43:53 +0300

    laba3

---
+++ doska.js
@@ -0,0 +1,57 @@
+﻿// 4/2/1
+let str;
+let n = 10;
+let blue = 9;
+
+document.write("<table>");
+for (let i = 0; i < n; i++) {
+    blue--
+    document.write("<tr>");
+    for (let j = 0; j < n; j++) {
+        if (j < blue) {
+            str = "class='r1'"; // в серый
+        } else {
+            str = "class='r2'";
+        }
+        document.write("<td " + str + "></td>");
+    }
+    document.write("</tr>");
+}
+document.write("</table>");
+
+
+// 5/18
+function sumSecondHalfDigits(number) {
+    let numToString = number.toString();
+    console.log(`numToString = ${numToString}`)
+    let secondHalf = numToString.slice(Math.floor(numToString.length / 2));
+    console.log(`secondHalf = ${secondHalf}`)
+
+    let sum = 0;
+    for (let el of secondHalf) {
+        console.log(`el = ${el}: sum = ${sum}`)
+        sum += Number(el);
+    }
+    return sum;
+}
+
+// console.log("Вывод процесса работы кода для задания 5/18")
+// let number = prompt("Задайте число для задания 18");
+// console.log(sumSecondHalfDigits(number));
+
+
+function findKthDigit(k) {
+    let sequence = "";
+    let num = 2;
+
+    while (sequence.length < k) {
+        sequence += num;
+        num += 2;
+    }
+    console.log(sequence)
+
+    return sequence[k - 1];
+}
+
+// let number = prompt("Задайте число K");
+// console.log(findKthDigit(number));
---
+++ index.js
@@ -0,0 +1,46 @@
+﻿function isValidNumber(value) {
+    if (isNaN(value) || value === null) {
+        return false;
+    }
+    return true;
+}
+
+let x = +prompt("Введите первое число (x):");
+if (!isValidNumber(x)) {
+    alert("Ошибка: Первое число введено некорректно.");
+} else {
+    let y = +prompt("Введите второе число (y):");
+    if (!isValidNumber(y)) {
+        alert("Ошибка: Второе число введено некорректно.");
+    } else {
+        let operation = prompt("Введите знак арифметической операции (+, -, *, /):");
+        let result;
+
+        switch (operation) {
+            case '+':
+                result = x + y;
+                break;
+            case '-':
+                result = x - y;
+                break;
+            case '*':
+                result = x * y;
+                break;
+            case '/':
+                if (y === 0) {
+                    alert("Деление на ноль невозможно.");
+                    result = null;
+                } else {
+                    result = x / y;
+                }
+                break;
+            default:
+                alert("Ошибка: Неверный знак арифметической операции.");
+                result = null;
+        }
+
+        if (result !== null) {
+            alert("Результат: " + result);
+        }
+    }
+}
